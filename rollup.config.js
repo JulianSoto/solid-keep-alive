ec6c94e544e98db74aac5845c73138dd79937929
Julian Soto <juliansotoponce@gmail.com>
2022-06-26 01:47:16 -0700

    chore: set rollup to work with babel and ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'src/index.jsx',
+  input: 'src/index.tsx',
   output: {
     dir: 'dist',
     format: 'es',
@@ -11,6 +13,11 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve(),
-    babel({ babelHelpers: 'bundled', presets: ['solid'] }),
+    typescript(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['solid'],
+      extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+    }),
   ],
 };
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { ParentProps } from 'solid-js';
+
+export default (props: ParentProps) => {
+  return <>{props.children}</>;
+};
