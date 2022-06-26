e54b3036b54edef5296de51cfcc3cd6d40f9cde9
Julian Soto <juliansotoponce@gmail.com>
2022-06-26 01:11:25 -0700

    feat: configure rollup to bundle with babel and jsx

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 export default {
-  input: 'src/index.js',
+  input: 'src/index.jsx',
   output: {
     dir: 'dist',
     format: 'es',
   },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    babel({ babelHelpers: 'bundled', presets: ['solid'] }),
+  ],
 };
