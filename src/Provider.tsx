28ce4e6c6d6df7b625599e9956f68caf456197f6
Julian Soto <juliansotoponce@gmail.com>
2022-06-26 16:20:47 -0700

    feat: create KeepAliveContext

---
+++ src/Provider.tsx
@@ -0,0 +1,60 @@
+import {
+  createContext,
+  createSignal,
+  JSX,
+  ParentProps,
+  useContext,
+} from 'solid-js';
+import { Accessor, Owner } from 'solid-js/types/reactive/signal';
+
+interface KeepAliveElement {
+  id: string;
+  owner: Owner | null;
+  children: JSX.Element;
+  dispose: () => void;
+}
+
+type Store = [
+  Accessor<KeepAliveElement[]>,
+  {
+    insertElement: (element: KeepAliveElement) => void;
+    removeElement: (id: string) => void;
+  }
+];
+
+const KeepAliveContext = createContext<Store>([
+  () => [],
+  {
+    insertElement: () => void 0,
+    removeElement: () => void 0,
+  },
+]);
+
+const KeepAliveProvider = (props: ParentProps) => {
+  const [keepAliveElements, setKeepAliveElements] = createSignal<
+    KeepAliveElement[]
+  >([]);
+
+  const insertElement = (element: KeepAliveElement) => {
+    setKeepAliveElements((prev) => [...prev, element]);
+  };
+
+  const removeElement = (id: string) => {
+    setKeepAliveElements((prev) => prev.filter((el) => el.id !== id));
+  };
+
+  const store: Store = [keepAliveElements, { insertElement, removeElement }];
+
+  return (
+    <KeepAliveContext.Provider value={store}>
+      {props.children}
+    </KeepAliveContext.Provider>
+  );
+};
+
+const useKeepAlive = () => {
+  return useContext(KeepAliveContext);
+};
+
+export { KeepAliveProvider };
+export { useKeepAlive };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { ParentProps } from 'solid-js';
+import { KeepAliveProvider } from './Provider';
 
 export default (props: ParentProps) => {
   return <>{props.children}</>;
 };
+
+export { KeepAliveProvider };
