33ee086b2d9597145538fc6ddd9cf85db090fe45
Julian Soto <juliansotoponce@gmail.com>
2022-06-26 22:05:32 -0700

    chore: export useKeepAlive from Provider component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import { KeepAliveProvider } from './Provider';
+import { KeepAliveProvider, useKeepAlive } from './Provider';
 import KeepAlive from './KeepAlive';
 
 export { KeepAlive };
 export { KeepAliveProvider };
+export { useKeepAlive };
