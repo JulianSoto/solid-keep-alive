0bfcfecd1a2caf5d14d21b8bcfbbacf4030e5dd3
Julian Soto <juliansotoponce@gmail.com>
2022-06-27 17:37:12 -0700

    docs(examples): render buttons to hide, show and unmount Counter

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,11 +1,29 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal, Show } from 'solid-js';
 import { KeepAlive } from '../../src';
+import { useKeepAlive } from '../../src/Provider';
+import Counter from './Counter';
 
 const App: Component = () => {
+  const [, { removeElement }] = useKeepAlive();
+  const [show, setShow] = createSignal(true);
+
   return (
     <div>
       <h1>solid-keep-alive examples</h1>
-      <KeepAlive id="keep-alive">Inside of {'<KeepAlive />'}</KeepAlive>
+      <button onclick={() => removeElement('keep-alive--counter')}>
+        Unmount counter
+      </button>
+      <button onclick={() => setShow(false)} disabled={!show()}>
+        Hide counter
+      </button>
+      <button onclick={() => setShow(true)} disabled={show()}>
+        Show counter
+      </button>
+      <Show when={show()}>
+        <KeepAlive id="keep-alive--counter">
+          <Counter />
+        </KeepAlive>
+      </Show>
     </div>
   );
 };
